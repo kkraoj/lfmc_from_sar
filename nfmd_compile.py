c6140c77c75260179af639923a7f5342ba597d8c
kkraoj <kkraoj@gmail.com>
2019-05-24 19:36:49 -0700

    updates

--- nfmd_compile.py
+++ nfmd_compile.py
@@ -13,11 +13,11 @@ from dirs import dir_data
 
 os.chdir(dir_data)
 df = pd.DataFrame()
-
-for file in os.listdir('fuel_moisture/raw_29_apr_2019'):
-    df = df.append([pd.read_table('fuel_moisture/raw_29_apr_2019/'+file)], ignore_index = True)
+date = '24_may_2019'
+for file in os.listdir('fuel_moisture/raw_%s'%date):
+    df = df.append([pd.read_table('fuel_moisture/raw_%s/'%date+file)], ignore_index = True)
 df.drop('Unnamed: 7', axis = 1, inplace = True)
 df.columns = df.columns.str.lower()
-df.to_pickle('fmc_04-29-2019')
+df.to_pickle('fmc_%s'%date)
 #df.drop_duplicates(subset = 'site').drop(['fuel','percent','date'], axis = 1).to_csv('fuel_moisture/site_info_query_10-16-2018.csv')
 
