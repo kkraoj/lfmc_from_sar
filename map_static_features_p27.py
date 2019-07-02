b1203743dbdfcf20cc64d6defcf6ead499aecc6b
kkraoj <kkraoj@gmail.com>
2019-07-01 18:36:20 -0700

    lat lon in roi and static maps

--- map_static_features_p27.py
+++ map_static_features_p27.py
@@ -17,41 +17,48 @@ import matplotlib.pyplot as plt
 
 os.chdir('D:/Krishna/projects/vwc_from_radar')
 
-#### adding different static features
-#latlon = pd.read_csv('data/map/map_lat_lon.csv', index_col = 0)
-#def get_value(filename, mx, my):
-#    ds = gdal.Open(filename)
-#    gt = ds.GetGeoTransform()
-#    data = ds.GetRasterBand(1).ReadAsArray()
-#    px = ((mx - gt[0]) / gt[1]).astype(int) #x pixel
-#    py = ((my - gt[3]) / gt[5]).astype(int) #y pixel
-#    return data[py,px]
-#
-#latlon = latlon[latlon.longitude !=0]
-#latlon['slope'] = get_value(os.path.join('D:\Krishna\Project\data\RS_data\Elevation\Elevation',\
-#                            'usa_slope_project.tif'), \
-#    latlon.longitude.values, latlon.latitude.values)
-#
-#### add elevation
-#latlon['elevation'] = get_value(os.path.join('D:\Krishna\Project\data\RS_data\Elevation\Elevation',\
-#                            'usa_dem.tif'), \
-#    latlon.longitude.values, latlon.latitude.values)
-#latlon['canopy_height'] = get_value(os.path.join('D:\Krishna\Project\data\RS_data\canopy_height',\
-#                            'canopy_height.tif'), \
-#    latlon.longitude.values, latlon.latitude.values)
-#
-#latlon['forest_cover'] = get_value(os.path.join('D:\Krishna\Project\data\RS_data\Forest\GLOBCOVER',\
-#                            'GLOBCOVER_L4_200901_200912_V2.3.tif'), \
-#    latlon.longitude.values, latlon.latitude.values)
-#
-#for col in ['silt','sand','clay']:   
-#    latlon['%s'%col] =\
-#      get_value(os.path.join('D:\Krishna\Project\data\RS_data\soil\NACP_MSTMIP_UNIFIED_NA_SOIL_MA_1242\data',\
-#      'Unified_NA_Soil_Map_Topsoil_%s_Fraction.tif'%col.capitalize()), \
-#        latlon.longitude.values, latlon.latitude.values)
-#latlon.head()
-#latlon.to_csv('data/map/static_features.csv')
+### adding different static features
+latlon = pd.read_csv('data/map/map_lat_lon.csv', index_col = 0)
+def get_value(filename, mx, my):
+    ds = gdal.Open(filename)
+    gt = ds.GetGeoTransform()
+    data = ds.GetRasterBand(1).ReadAsArray()
+    px = ((mx - gt[0]) / gt[1]).astype(int) #x pixel
+    py = ((my - gt[3]) / gt[5]).astype(int) #y pixel
+    return data[py,px]
+#%% add static features
+latlon = latlon[latlon.longitude !=0]
+latlon['slope'] = get_value(os.path.join('D:\Krishna\Project\data\RS_data\Elevation\Elevation',\
+                            'usa_slope_project.tif'), \
+    latlon.longitude.values, latlon.latitude.values)
 
+### add elevation
+latlon['elevation'] = get_value(os.path.join('D:\Krishna\Project\data\RS_data\Elevation\Elevation',\
+                            'usa_dem.tif'), \
+    latlon.longitude.values, latlon.latitude.values)
+latlon['canopy_height'] = get_value(os.path.join('D:\Krishna\Project\data\RS_data\canopy_height',\
+                            'canopy_height.tif'), \
+    latlon.longitude.values, latlon.latitude.values)
+
+latlon['forest_cover'] = get_value(os.path.join('D:\Krishna\Project\data\RS_data\Forest\GLOBCOVER',\
+                            'GLOBCOVER_L4_200901_200912_V2.3.tif'), \
+    latlon.longitude.values, latlon.latitude.values)
+
+for col in ['silt','sand','clay']:   
+    latlon['%s'%col] =\
+      get_value(os.path.join('D:\Krishna\Project\data\RS_data\soil\NACP_MSTMIP_UNIFIED_NA_SOIL_MA_1242\data',\
+      'Unified_NA_Soil_Map_Topsoil_%s_Fraction.tif'%col.capitalize()), \
+        latlon.longitude.values, latlon.latitude.values)
+
+latlon.head()
+latlon.to_csv('data/map/static_features.csv')
+
+#%%add dynamic features
+
+
+
+
+#%%
 ######making map
 
 def plot_map(lats=None,lons=None, var =None,\
